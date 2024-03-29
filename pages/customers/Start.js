fd3d6122b693bf2fb34901c69ac4c59f7b4dbc11
unknown <konankanjulius10@gmail.com>
2024-03-29 09:11:48 +0000

    first page

--- pages/customers/Start.js
+++ pages/customers/Start.js
@@ -4,22 +4,21 @@ import { AntDesign } from '@expo/vector-icons';
 import React from 'react'
 
 const Start = () => {
-    const image = require("../../assets/images/first.png");
+    const image = require("../../assets/images/background/first.png");
   return (
-    <View style={{ backgroundColor: "#DE9F42" }}>
+    <View style={{ backgroundColor: "#fff" }}>
       <View>
         <ImageBackground source={image} style={{width: "auto", height: 800}} resizeMode="cover">
-            <View style={{backgroundColor: '#47300D', width: 400, height: 200,borderRadius:150, marginLeft:-80, marginTop: -55 }}></View>
-            <View style={{ backgroundColor: "#47300D", width:268, height:189,flexDirection: 'row', flexWrap: 'wrap', justifyContent: "center", alignItems:'center', marginTop: -5, borderTopRightRadius: 5060,borderBottomRightRadius: 5060}}>
+            <View style={{ width: 400, height: 200,borderRadius:150, marginLeft:-80, marginTop: -55 }}></View>
+            <View style={{ width:268, height:189,flexDirection: 'row', flexWrap: 'wrap', justifyContent: "center", alignItems:'center', marginTop: -5, borderTopRightRadius: 5060,borderBottomRightRadius: 5060}}>
                 <Text style={styles.textStyle}>A CHACUN SON "MANAWA"</Text>
             </View>
             
-            <View style={{ width:250, height:200, backgroundColor:"#47300D", borderRadius:50, borderRadius:150, marginLeft:-80, marginTop: -10 }}></View>
-            <View style={{ width:200, height:200, backgroundColor:"#47300D", borderRadius:50, borderRadius:150, marginLeft:-80, marginTop: -10 }}></View>
-            <View style={{backgroundColor: '#47300D', width: 50, height: 90, borderTopRightRadius:150, marginTop:-10, borderBottomRightRadius:100, marginLeft:-18 }}></View>
-            {/* <View style={{backgroundColor: '#47300D', width: 300, height: 220,borderTopRightRadius:200, marginTop:-10, borderBottomRightRadius:100 }}></View> */}
+            <View style={{ width:250, height:200,  borderRadius:50, borderRadius:150, marginLeft:-80, marginTop: -10 }}></View>
+            <View style={{ width:200, height:200,  borderRadius:50, borderRadius:150, marginLeft:-80, marginTop: -10 }}></View>
+            <View style={{ width: 50, height: 90, borderTopRightRadius:150, marginTop:-10, borderBottomRightRadius:100, marginLeft:-18 }}></View>
             <View style={{width: "auto", height: 53, alignItems: "flex-end", margin: 30, marginTop: -80, }}>
-                <Button title="COMMENCER " color="#DE9F42" />
+                <Button title="COMMENCEr " color="#DE9F42" />
             </View>
         </ImageBackground>
       </View>
