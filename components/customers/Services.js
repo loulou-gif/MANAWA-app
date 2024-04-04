689154b0ca975a0cfb772a956d5e738ac4dd5b08
unknown <konankanjulius10@gmail.com>
2024-04-04 00:05:20 +0000

    first composent integration manawa pages

--- components/customers/Services.js
+++ components/customers/Services.js
@@ -1,13 +1,19 @@
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
+import { services } from '../../data/services'
 
 const Services = () => {
   return (
     <View>
       <View style={styles.center} >
-        <ImageBackground style={styles.Card} >
+        {services.map((data) =>(
+          <ImageBackground key={data.id}  style={styles.Card} source={require(data.image)}>
+              <View style={styles.titleBox} >
+                  <Text style={styles.title} >{data.name}</Text>
+              </View>
+          </ImageBackground>
+        ))}
         
-        </ImageBackground>
       </View>
     </View>
   )
@@ -19,14 +25,30 @@ const styles = StyleSheet.create({
     height:150,
     borderRadius:8,
     shadowColor:"black",
-    boxShadow:5,
-    backgroundColor:'red',
+    boxShadow:20,
+    backgroundColor:'black',
+    resizeMode:"cover",
     marginTop:20,
     // alignContent:"center",
     // justifyContent:"center"
   },
   center:{
     alignItems:"center",
+  },
+  title:{
+    fontSize:20,
+    color:"#fff",
+    marginLeft: 10
+  },
+  titleBox:{
+    justifyContent:"center",
+    backgroundColor:"#F3F3F3",
+    opacity: 0.6,
+    width:300,
+    height:40,
+    marginTop:110,
+    borderBottomEndRadius:8
+
   }
 })
 
--- data/Services.js
+++ data/Services.js
@@ -1,7 +1,29 @@
+
+
 export const services = [
     {
         id:1,
-        image:"",
-        name:"", 
-    }
+        image:"../assets/images/servicesBackgrounds/menuisier.jpg",
+        name:"Menuisier", 
+    },
+    {
+        id:2,
+        image:"../assets/images/servicesBackgrounds/barbershop.jpg",
+        name:"Barber", 
+    },
+    {
+        id:3,
+        image:"../assets/images/servicesBackgrounds/plombier.jpg",
+        name:"Plomber", 
+    },
+    {
+        id:4,
+        image:"../assets/images/servicesBackgrounds/jardinier.jpg",
+        name:"Jardinier", 
+    },
+    {
+        id:5,
+        image:"../assets/images/servicesBackgrounds/maquilliage.jpg",
+        name:"Make Up", 
+    },
 ]
