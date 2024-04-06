6f14a8184e7127d073780e366921b953c4837091
unknown <konankanjulius10@gmail.com>
2024-04-06 00:00:17 +0000

    simple modif

--- components/customers/StoreHeader.js
+++ components/customers/StoreHeader.js
@@ -5,8 +5,19 @@ const StoreHeader = () => {
   return (
     <View>
       <Image style={styles.image} source={require("../../assets/images/servicesBackgrounds/babershop.jpg")}/>
-      <View>
-        
+      <View style={styles.bottom}>
+        <View style={styles.flex}>
+            <Image source={require('../../assets/images/Profils/p8.png')} />
+            <View styles={styles.text}>
+                <Text style={styles.title}>Salon de coiffure</Text>
+                <Text style={styles.state}>Ouvert (09:00 - 20:30)</Text>
+            </View>
+        </View>
+        <View style={styles.flex} >
+            <Text styles={styles.menuMain}>Service</Text>
+            <Text styles={styles.menu}>Locaux</Text>
+            <Text styles={styles.menu}>Avis</Text>
+        </View>
       </View>
     </View>
   )
@@ -17,6 +28,34 @@ const styles = StyleSheet.create({
     image:{
         width:500,
         height:120
+    },
+    state:{
+        color:"green",
+        fontSize:12,
+    },
+    title:{
+        fontSize:20,
+    },
+    flex:{
+        flexDirection: "row",
+        justifyContent:"space-evenly",
+        width:300,
+        marginTop:15,
+        marginBottom:5,
+    },
+    bottom:{
+        borderBottomWidth: 1,
+        borderBlockColor:'#ABA9A9',
+    },
+    text:{
+        marginBottom:50,
+    },
+    menu:{
+        fontSize:16,
+    },
+    menuMain:{
+        fontSize:16,
+        color:"#DE9F42"
     }
 })
 
