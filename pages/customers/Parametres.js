7a658ec74b2fbcdb1ffbcde1404c2a8b4ddcd1fc
unknown <konankanjulius10@gmail.com>
2024-04-12 00:21:37 +0000

    parametres

--- pages/customers/Parametres.js
+++ pages/customers/Parametres.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Header from '../../components/customers/Header' 
 
 const Parametres = () => {
   return (
     <View>
-      <Text>Parametres</Text>
+      <Header/>
+      <View style={styles.circle}>
+        <Text style={styles.initial}>JK</Text>
+      </View>
+      <Text>Comptes</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  circle:{
+    width:100,
+    height:100,
+    borderRadius:50,
+    backgroundColor:"#E520DD",
+    marginTop:50,
+    marginLeft:20,
+  },
+  initial:{
+    fontSize:30,
+    color:"#fff",
+    textAlign:'center',
+    // alignContent:'center',
+    alignItems:'center',
+    marginTop:30
+  }
+})
 
 export default Parametres
