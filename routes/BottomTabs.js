02ae7153020c0ca7180a026cf1ce42ff47eb2fbf
unknown <konankanjulius10@gmail.com>
2024-04-12 00:23:08 +0000

    parametres

--- routes/BottomTabs.js
+++ routes/BottomTabs.js
@@ -4,15 +4,16 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import Manawa from '../pages/customers/Manawa';
 import Prestataires from '../pages/customers/Prestataires';
 import Panier from '../pages/customers/Panier';
-// import Owner from '../pages/customers/Owner';
+import Parametres from '../pages/customers/Parametres';
 
 const BottomTabs = () => {
     const Tab = createMaterialBottomTabNavigator()
   return (
-    <Tab.Navigator initialRouteName='Manawa'  screenOptions={{headerShown: false}} activeColor="#fff" inactiveColor="#F8F7F7" barStyle={{ backgroundColor: '#47300D', height:80 }}>
-        <Tab.Screen name='Services' component={Manawa}/>
+    <Tab.Navigator initialRouteName='Manawa'  screenOptions={{headerShown: false}} activeColor="#fff" inactiveColor="#F8F7F7" barStyle={{ backgroundColor: '#47300D', height:80, borderTopEndRadius:50 }}>
         <Tab.Screen name='Manawa' component={Prestataires}/>
+        <Tab.Screen name='Services' component={Manawa}/>
         <Tab.Screen name='Panier' component={Panier}/>
+        <Tab.Screen name='Comptes' component={Parametres}/>
     </Tab.Navigator>
   )
 }
